302aaa4ac0fe7c4451f3cc7b1dc3f64206e8a86f
Francois Budin <francois.budin@kitware.com>
2017-06-29 22:31:52 -0400

    Download pre-compiled FFTW on Windows and MacOS

--- SuperBuild/External_FFTW.cmake
+++ SuperBuild/External_FFTW.cmake
@@ -22,27 +22,40 @@ if(NOT DEFINED ${proj}_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
         set(git_protocol "git")
     endif()
 
+    set(FFTW_CONFIGURE_COMMAND "")
+    set(FFTW_BUILD_COMMAND "")
+    set(FFTW_INSTALL_COMMAND "")
+
+
     if(WIN32)
         # Assume that Slicer is always built in 64bits on Windows
-        set(ftp://ftp.fftw.org/pub/fftw/fftw-3.3.5-dll64.zip)
+        set(DOWNLOAD_URL ftp://ftp.fftw.org/pub/fftw/fftw-3.3.5-dll64.zip)
+    elseif(APPLE)
+        # See Utilies/README to generate this package
+        set(DOWNLOAD_URL "http://slicer.kitware.com/midas3/download/?items=296662,1")
+        
+        set(INSTALL_LIBRARIES
+        ${${proj}_ROOT}/lib/libfftw3.3.dylib
+        ${${proj}_ROOT}/lib/libfftw3.dylib
+        ${${proj}_ROOT}/lib/libfftw3.la
+        ${${proj}_ROOT}/lib/libfftw3_threads.3.dylib
+        ${${proj}_ROOT}/lib/libfftw3_threads.dylib
+        ${${proj}_ROOT}/lib/libfftw3_threads.la
+    )
+        
     else()
+        set(FFTW_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/FFTW)
         set(DOWNLOAD_URL http://fftw.org/fftw-3.3.6-pl2.tar.gz)
-    endif()
 
-    set(FFTW_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/FFTW)
-    set(FFTW_build ${CMAKE_CURRENT_BINARY_DIR}/FFTW-build)
-    include(ExternalProjectForNonCMakeProject)
+        include(ExternalProjectForNonCMakeProject)
 
-    set(tcl_SOURCE_DIR FFTW)
-    set(${proj}_BUILD_IN_SOURCE 1)
-
-    # environment
-    set(_env_script ${CMAKE_BINARY_DIR}/${proj}_Env.cmake)
-    ExternalProject_Write_SetBuildEnv_Commands(${_env_script})
-
-    # configure step
-    set(_configure_script ${CMAKE_BINARY_DIR}/${proj}_configure_step.cmake)
-    file(WRITE ${_configure_script}
+        # environment
+        set(_env_script ${CMAKE_BINARY_DIR}/${proj}_Env.cmake)
+        ExternalProject_Write_SetBuildEnv_Commands(${_env_script})
+    
+        # configure step
+        set(_configure_script ${CMAKE_BINARY_DIR}/${proj}_configure_step.cmake)
+        file(WRITE ${_configure_script}
 "include(\"${_env_script}\")
 set(${proj}_WORKING_DIR \"${FFTW_SOURCE_DIR}\")
 ExternalProject_Execute(${proj} \"configure\" sh configure
@@ -50,26 +63,36 @@ ExternalProject_Execute(${proj} \"configure\" sh configure
   --enable-shared --enable-static=no
   )
 ")
-    set(FFTW_CONFIGURE_COMMAND ${CMAKE_COMMAND} -P ${_configure_script})
 
-    # build step
-    set(_build_script ${CMAKE_BINARY_DIR}/${proj}_build_step.cmake)
-    file(WRITE ${_build_script}
+        # build step
+        set(_build_script ${CMAKE_BINARY_DIR}/${proj}_build_step.cmake)
+        file(WRITE ${_build_script}
 "include(\"${_env_script}\")
 set(${proj}_WORKING_DIR \"${FFTW_SOURCE_DIR}\")
 ExternalProject_Execute(${proj} \"build\" make)
 ")
-    set(FFTW_BUILD_COMMAND ${CMAKE_COMMAND} -P ${_build_script})
 
-    # install step
-    set(_install_script ${CMAKE_BINARY_DIR}/${proj}_install_step.cmake)
-    file(WRITE ${_install_script}
+        # install step
+        set(_install_script ${CMAKE_BINARY_DIR}/${proj}_install_step.cmake)
+        file(WRITE ${_install_script}
 "include(\"${_env_script}\")
 set(${proj}_WORKING_DIR \"${FFTW_SOURCE_DIR}\")
 ExternalProject_Execute(${proj} \"install\" make install)
 ")
-    set(FFTW_INSTALL_COMMAND ${CMAKE_COMMAND} -P ${_install_script})
 
+        set(FFTW_CONFIGURE_COMMAND ${CMAKE_COMMAND} -P ${_configure_script})
+        set(FFTW_BUILD_COMMAND ${CMAKE_COMMAND} -P ${_build_script})
+        set(FFTW_INSTALL_COMMAND ${CMAKE_COMMAND} -P ${_install_script})
+
+        set(INSTALL_LIBRARIES
+            ${${proj}_ROOT}/lib/libfftw3.so
+            ${${proj}_ROOT}/lib/libfftw3.so.3
+            ${${proj}_ROOT}/lib/libfftw3.so.3.5.6
+        )
+    endif()
+    
+    set(FFTW_build ${CMAKE_CURRENT_BINARY_DIR}/FFTW-build)
+    set(${proj}_BUILD_IN_SOURCE 1)
 
     ExternalProject_Add(${proj}
         ${${proj}_EP_ARGS}
@@ -84,11 +107,6 @@ ExternalProject_Execute(${proj} \"install\" make install)
           ${${proj}_DEPENDS}
     )
     set(${proj}_ROOT ${CMAKE_BINARY_DIR}/${proj}-build)
-    set(INSTALL_LIBRARIES
-        ${${proj}_ROOT}/lib/libfftw3.so
-        ${${proj}_ROOT}/lib/libfftw3.so.3
-        ${${proj}_ROOT}/lib/libfftw3.so.3.5.6
-    )
 else()
   ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDS})
 endif()
